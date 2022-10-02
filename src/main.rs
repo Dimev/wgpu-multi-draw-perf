768eba1cbd087448a8bb082efd778ea660778632
Dimas Leenman <skythedragon@outlook.com>
2022-10-02 13:19:13 +0200

    now more done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+
+#[repr(C)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable, Copy, Clone)]
+struct Uniform {
+    modelview: [[f32; 4]; 4],
+}
+
+#[repr(C)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable, Copy, Clone)]
+struct Vertex {
+    pos: [f32; 3],
+}
+
+#[repr(C)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable, Copy, Clone)]
+struct Object {
+    color: [f32; 3],
+    pos: [f32; 3],
+    transform: [[f32; 3]; 3],
+}
+
+#[repr(C)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable, Copy, Clone)]
+struct DrawIndirect {
+    vertex_count: u32,
+    instance_count: u32,
+    base_index: u32,
+    vertex_offset: u32,
+    base_instance: u32,
+}
+
+fn generate_mesh() -> (Object, Vec<Vertex>, Vec<u16>) {
+    todo!();
+}
 
 fn main() {
     // enable multi draw if needed
@@ -44,7 +79,7 @@ fn main() {
             label: None,
             limits: wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits()),
             features: if multidraw_allowed {
-                wgpu::Features::MULTI_DRAW_INDIRECT
+                wgpu::Features::MULTI_DRAW_INDIRECT | wgpu::Features::INDIRECT_FIRST_INSTANCE
             } else {
                 wgpu::Features::empty()
             },
@@ -80,25 +115,27 @@ fn main() {
             0 => Float32x3,
         ],
     };
-    
-    
+
     // per mesh (this is done in instance rate vertex buffers)
     let object_layout = wgpu::VertexBufferLayout {
         array_stride: 3 * 4,
         step_mode: wgpu::VertexStepMode::Vertex,
         attributes: &wgpu::vertex_attr_array![
             1 => Float32x3,
-            // ???
+            2 => Float32x3,
+            3 => Float32x3,
+            4 => Float32x3,
+            5 => Float32x3,
         ],
-    };    
-    
+    };
+
     // set up pipeline
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
         bind_group_layouts: &[&uniform_bind_group_layout],
         push_constant_ranges: &[],
     });
-    
+
     // pipeline
     let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: None,
@@ -114,14 +151,79 @@ fn main() {
             targets: &[Some(wgpu::TextureFormat::Bgra8UnormSrgb.into())],
         }),
         primitive: wgpu::PrimitiveState {
-            cull_mode: Some(wgpu::Face::Back),        
+            cull_mode: Some(wgpu::Face::Back),
             ..Default::default()
         },
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
     });
-    
+
     // buffers
+    let mut indirect_draw = Vec::new();
+    let mut vertices = Vec::new();
+    let mut objects = Vec::new();
+    let mut indices = Vec::new();
+
+    // generate some objects
+    // TODO: tunable size
+    for i in 0..1024 {
+        // generate the mesh
+        let (object, verts, tris) = generate_mesh();
+
+        // append
+        indirect_draw.push(DrawIndirect {
+            vertex_count: verts.len() as u32,
+            instance_count: 1,
+            base_index: indices.len() as u32,
+            vertex_offset: vertices.len() as u32,
+            base_instance: objects.len() as u32,
+        });
+
+        // append
+        vertices.extend(verts);
+        indices.extend(tris);
+        objects.push(object);
+    }
+
+    // create uniform
+    let mut uniforms = Uniform {
+        modelview: [[0.0; 4]; 4],
+    };
+
+    // make wgpu buffers
+    let uniforms_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("uniforms"),
+        usage: wgpu::BufferUsages::UNIFORM,
+        contents: bytemuck::cast_slice(&[uniforms]),
+    });
+
+    let indirect_draw_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("indirect draw"),
+        usage: wgpu::BufferUsages::INDIRECT,
+        contents: bytemuck::cast_slice(&indirect_draw),
+    });
+
+    let vertices_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("vertices"),
+        usage: wgpu::BufferUsages::VERTEX,
+        contents: bytemuck::cast_slice(&vertices),
+    });
+
+    let indices_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("indices"),
+        usage: wgpu::BufferUsages::INDEX,
+        contents: bytemuck::cast_slice(&indices),
+    });
+
+    let objects_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("objects"),
+        usage: wgpu::BufferUsages::VERTEX,
+        contents: bytemuck::cast_slice(&objects),
+    });
     
+    // start the event loop
+    event_loop.run(move |event, _, &mut control_flow| {
+    
+    });
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -11,15 +11,24 @@ struct VertOut {
 fn vertex(
     @location(0) vert: vec3<f32>,
     @location(1) obj_color: vec3<f32>,
-    @location(2) obj_transform: mat4x4<f32>,
-) -> VertexOut {
-    var vert: VertexOut;
-    vert.position = vert;
+    @location(2) obj_pos: vec3<f32>,
+    @location(3) obj_x: vec3<f32>,
+    @location(4) obj_y: vec3<f32>,
+    @location(5) obj_z: vec3<f32>,
+) -> VertOut {
+
+    // transform
+    let world_vert = vec3<f32>(dot(vert, obj_x), dot(vert, obj_y), dot(vert, obj_z)) + obj_pos;
+    let view_vert = vec4<f32>(world_vert, 1.0) * modelview;
+
+    // emit vertex
+    var vert: VertOut;
+    vert.position = vec4<f32>(view_vert.xyz, 1.0);
     vert.obj_color = obj_color;
     return vert;
 }
 
-@framgent
+@fragment
 fn fragment(vertex: VertOut) -> @location(0) vec4<f32> {
     return vec4<f32>(vertex.obj_color, 1.0);
 }
